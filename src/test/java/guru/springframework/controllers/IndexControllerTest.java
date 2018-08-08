1bb64142e56c222155410daf2e0de7752ab13841
Aaron Smith <aaron.smith@kuehne-nagel.com>
2018-08-08 15:11:55 +0100

    Completed lecture 165/166 - Write test for IndexController.

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import guru.springframework.services.RecipeService;
+
+public class IndexControllerTest {
+
+	IndexController controller;
+	
+	@Mock
+	RecipeService recipeService;
+	
+	@Mock
+	Model model;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		controller = new IndexController(recipeService);
+	}
+
+	@Test
+	public void testGetIndexPage() {
+		String viewName = controller.getIndexPage(model);
+		assertEquals(viewName, "index");
+		
+		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(recipeService, times(1)).getRecipes();
+	}
+
+}
